5db72f6e162f59fb92036c6e6ec9cf17c67b7228
Walberth <w.felipe.gutierrez@gmail.com>
2019-07-21 16:17:07 -0500

    se corrigen los archivos de los proyectos subidos hasta el momento

---
+++ SOLID/SingleResponsabilityPrinciple/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SingleResponsabilityPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: LOGGING
+            Console.WriteLine($"Start consuming the method: {DateTime.Now}");
+
+
+            // TODO: VALIDATION
+
+            //TODO: BUSSINES LOGIC
+
+            // TODO: PERSISTENCE
+
+            // TODO: LOGGING
+        }
+    }
+}
